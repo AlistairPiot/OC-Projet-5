68d33d958c9762fae89da906c347d4bc12615f3f
Alistair_Piot <alistair.pro15@gmail.com>
2023-12-06 19:24:17 +0100

    ajout EventListerner pour les deux flèches

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,33 @@
 const slides = [
-	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
-	},
-	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
-	},
-	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
-	},
-	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
-	}
-]
+    {
+        image: "slide1.jpg",
+        tagLine:
+            "Impressions tous formats <span>en boutique et en ligne</span>",
+    },
+    {
+        image: "slide2.jpg",
+        tagLine:
+            "Tirages haute définition grand format <span>pour vos bureaux et events</span>",
+    },
+    {
+        image: "slide3.jpg",
+        tagLine: "Grand choix de couleurs <span>de CMJN aux pantones</span>",
+    },
+    {
+        image: "slide4.png",
+        tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
+    },
+];
+
+const arrowLeft = document.querySelector(".arrow_left");
+const arrowRight = document.querySelector(".arrow_right");
+
+arrowLeft.addEventListener("click", function () {
+    console.log("Clic sur la flèche gauche");
+    // Ajoutez ici le code pour passer à la diapositive précédente
+});
+
+arrowRight.addEventListener("click", function () {
+    console.log("Clic sur la flèche droite");
+    // Ajoutez ici le code pour passer à la diapositive suivante
+});
